b1e5c502322f3a72b5182f3359ec1a23cd323d9b
nathaliaortega <nathalia.ortega@mercadolibre.com.co>
2023-03-03 10:00:56 -0500

    parragraph edit

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -19,16 +19,16 @@ function Home() {
             cuales son utilizados para medir la calidad de la producción entregada y para otros 
             procesos internos de la compañía, sin embargo, estos datos no son utilizados en 
             procesos como la minería de datos para la predicción de características del concreto 
-            como asentamiento, resistencia a la compresión o resistencia a la flexión.<br/> 
+            como asentamiento, resistencia a la compresión o resistencia a la flexión.<br/> <br/> 
             La minería de datos es una rama de la analítica que permite el descubrimiento de 
             información en grandes volúmenes de datos a través de técnicas predictivas que permite 
             predecir eventos futuros, y técnicas descriptivas que permiten encontrar relaciones 
-            en el comportamiento de las variables estudiadas.  <br/>
+            en el comportamiento de las variables estudiadas.  <br/><br/> 
             La herramienta MANUFACTURA INTELIGENTE DE LOS DATOS, es una herramienta que 
             permite realizar la predicción de la resistencia a la compresión, asentamiento
              y predicción de la resistencia a la flexión, basándose en los datos estructurados 
              provenientes de las bases de datos de concreto. Esta herramienta, facilita su 
-             implementación en el sector de la manufactura para experimentos, nuevos desarrollos o desarrollos existentes.<br/>
+             implementación en el sector de la manufactura para experimentos, nuevos desarrollos o desarrollos existentes.<br/><br/> 
 
             </p>
         </div>
